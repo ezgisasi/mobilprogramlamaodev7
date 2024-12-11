5a9eb66fb1e8f515fe01bce120475a2897d92521
Ezgi Şaşı <ezgi.sasi@bil.omu.edu.tr>
2024-12-11 08:09:20 +0300

    first commit

--- Backend/app.js
+++ Backend/app.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-const app = express();
 const port = 3000;
+const app = express();
 
 require('dotenv').config();
 
